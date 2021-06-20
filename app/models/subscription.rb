b38f11f52c5eb6c5a2bc50fbe5c2de5ccedccabd
Benjamin Baranger <baranger.benjamin@gmail.com>
2021-06-20 17:59:39 +0100

    validates frequency

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,4 @@
 class Subscription < ApplicationRecord
     attr_reader :duration
-    validates_presence_of  :name, :price, :start_at, :category
+    validates_presence_of  :name, :price, :start_at, :category, :frequency
 end
