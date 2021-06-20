a2318f6c5cbfacbf9909e1d49b5ebc60a4242bc8
Benjamin Baranger <baranger.benjamin@gmail.com>
2021-06-20 13:52:14 +0100

    add category to subs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_102724) do
+ActiveRecord::Schema.define(version: 2021_06_20_115006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
